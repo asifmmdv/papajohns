486716ea85021bff208b10a05ef82768f7f6ee32
asifmmdv <mamedov_asif@icloud.com>
2025-04-14 15:46:26 +0400

    sas

--- js/detail.js
+++ js/detail.js
@@ -115,20 +115,19 @@ function observeSections() {
 
 function openModal(item = null) {
     const modalContent = document.getElementById('modal-content');
-    document.body.style.overlowY = 'hidden';
     if (item) {
         currentItem = item;
         quantity = 1;
         modalContent.innerHTML = `
-            <div class="flex flex-col w-full bg-[rgb(246,246,246)] h-full">
+            <div class="flex flex-col w-full bg-[rgb(246,246,246)]">
                 <div onclick='modal.classList.toggle("hidden")' class="ml-auto mr-[12px] mt-[8px]">
                     <i class="fa-solid fa-xmark text-[28px]"></i>
                 </div>
-                <div class="flex flex-col justify-center items-center w-full h-full py-[20px]">
-                    <div class="flex flex-col ">
+                <div class="flex flex-col justify-center items-center w-full  py-[20px]">
+                    <div class="flex flex-col w-full justify-center items-center ">
                         <img src="${item.img}" class="w-[317px] h-[211px] rounded-[20px]">
                         <h2 class="text-[24px] mt-[20px] text-left font-bold">${item.title}</h2>
-                        <p class="text-[14px] mt-[8px]">${item.composition}</p>
+                        <p class="text-[14px] text-center mt-[8px]">${item.composition}</p>
                     </div>
                     <div class="flex items-center p-[24px] gap-4 text-nowrap justify-between mt-[280px]">
                         <div class="flex items-center gap-7 h-[60px]">
